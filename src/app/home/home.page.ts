b3e54dd6d10a1d355c4ea698aed8586afdec0307
marcosquadros <>
2020-11-16 22:53:55 -0300

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  public options: Array<any> = [
+    {icon: 'person-add-outline', text: 'Indicar amigos'},
+    {icon: 'phone-portrait-outline', text: 'Recarga de celular'},
+    {icon: 'wallet-outline', text: 'Depositar'},
+    {icon: 'options-outline', text: 'Ajustar Limite'},
+    {icon: 'help-circle-outline', text: 'Me ajuda'},
+    {icon: 'barcode-outline', text: 'Pagar'},
+    {icon: 'lock-open-outline', text: 'Bloquear cartão'},
+    {icon: 'card-outline', text: 'Cartão virtual'}
+  ]
+  
+  public slidesOptions: any = {slidesPerView: 3, freeMode:true}
+  public items: Array<any> = [
+    {icon: 'help-circle-outline',text: 'Me Ajuda'},
+    {icon: 'person-outline',text:'Perfil'},
+    {icon: 'cash-outline',text:'Configurar Conta'},
+    {icon: 'card-outline',text:'Configurar Cartão'},
+    {icon: 'phone-portrait-outline',text:'Configurações de App'},
+  ]
+  constructor() {}
+
+}
